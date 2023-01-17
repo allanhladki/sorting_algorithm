945e27994a8a69807cef2e790a0528e132a35fa3
allanhladki <allanhladki.ah@gmail.com>
2023-01-17 20:24:06 -0300

    Criacao da classe mãe com algumas funções básicas

--- src/SortingAlgorithms.java
+++ src/SortingAlgorithms.java
@@ -1,4 +1,39 @@
-
+// Classe mãe que servirá como base na implementação dos algoritmos
 public class SortingAlgorithms {
+	
+	private int[] unsortedArray; // Variável que armazena o vetor com os dados númericos;
+
+	public int[] getArrayEntrada() {
+		return this.unsortedArray;
+	}
+
+	public void setArrayEntrada(int[] array) {
+		this.unsortedArray = array;
+	}
+	
+	public int arrayLength() {
+		return this.unsortedArray.length;
+	}
+	
+	// Método para escrever no console o array de forma agradável
+	public void printArray() {
+		System.out.print("[");
+		for (int i = 0 ; i < arrayLength()-1 ; i++) {
+			System.out.print(this.unsortedArray[i] + " , ");
+		}
+		System.out.println(this.unsortedArray[arrayLength()-1] + "]");
+	}
+	
+	// Método para salvar o array em uma string
+	public String stringArray() {
+		String s = "[";
+		
+		for (int i = 0 ; i < arrayLength()-1 ; i++) {
+			s += (this.unsortedArray[i] + " , ");
+		}
+		s += (this.unsortedArray[arrayLength()-1] + "]");
+		
+		return s;
+	}
 
 }
--- src/SortingTest.java
+++ src/SortingTest.java
@@ -4,6 +4,18 @@ public class SortingTest {
 
 	public static void main(String[] args) {
 		
+		int[] array = {3,1,2,5,4,6,8,7};	// criação do array
+		
+		SortingAlgorithms sortArray = new SortingAlgorithms(); // criação de um objeto do tipo SortingAlgorithms
+		
+		sortArray.setArrayEntrada(array);
+		
+		System.out.println("IMPLEMENTAÇÃO INICIAL COM NÚMEROS INTEIROS");
+		System.out.println("O vetor tem " + sortArray.arrayLength() + " números");
+		System.out.print("1 - De forma não ordenada ele é ");
+		sortArray.printArray(); // Uma outra forma de mostrar esse vetor é a seguinte
+		System.out.print("2 - De forma não ordenada ele é " + sortArray.stringArray());
+		
 	}
 	
 }
